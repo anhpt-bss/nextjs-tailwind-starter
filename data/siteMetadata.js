05b36fe6eef3f3d561e07830b34c4dc7f4ea9838
anhpt <anhpt@basebs.com>
2025-05-26 14:08:01 +0700

    chore: Update app info

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,8 +9,8 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://nextjs-tailwind-starter-ecru.vercel.app',
   siteRepo: 'https://github.com/anhpt-bss/nextjs-tailwind-starter',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/app/logo.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/app/banner.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',
